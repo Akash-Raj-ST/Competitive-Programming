b9c74a1696aea88aa46fc7ee46f0fea45e164336
Akash Raj ST <akashraj49070@gmail.com>
2022-07-07 18:30:53 +0530

    linear chess

---
+++ codechef/LINCHESS.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LINCHESS {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t-->0){
+            int n = sc.nextInt();
+            int curr_pos = sc.nextInt();
+            int arr[] = new int[n];
+
+            for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+
+            int min=curr_pos,pos=-1;
+            for(int i=0;i<n;i++){
+                if(curr_pos%arr[i]==0){ 
+                    if(curr_pos/arr[i]<min){ 
+                        min = curr_pos/arr[i];
+                        pos = arr[i];
+                    };
+                }
+            }
+            System.out.println(pos);
+        }
+    }
+}
