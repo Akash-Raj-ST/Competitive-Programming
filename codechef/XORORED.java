e68c7e8c2d6db92ac5b8abfbf10d073aa6f82c80
Akash Raj ST <akashraj49070@gmail.com>
2022-07-07 18:22:10 +0530

    xorored

---
+++ codechef/XORORED.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class XORORED {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t-->0){
+            int n = sc.nextInt();
+
+            int arr[] = new int[n];
+            int and = -1;
+            for(int i=0;i<n;i++) {
+                arr[i] = sc.nextInt();
+                and = and&arr[i];
+            };
+
+            int sum = 0;
+            for(int i=0;i<n;i++){
+                sum = sum |( arr[i]^and);
+            }
+
+            System.out.println(and+" "+sum);
+        }
+    }
+}
