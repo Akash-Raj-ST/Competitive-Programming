74e5723a70ffba4dbedcfc2f596cbf2abc4d6069
Akash Raj ST <akashraj49070@gmail.com>
2022-07-08 10:23:31 +0530

    alter

---
+++ codechef/ALTER.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ALTER{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+
+            if(p%a==0 && q%b==0){
+                int diff = Math.abs((p/a)-(q/b));
+
+                if(diff<2) System.out.println("YES");
+                else System.out.println("NO");
+            }else System.out.println("NO");
+        }
+        sc.close();
+    }
+}
