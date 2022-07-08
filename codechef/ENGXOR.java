57e3cdaa0f9c4e3ccbbda049ec637d37461acdc8
Akash Raj ST <akashraj49070@gmail.com>
2022-07-08 11:24:37 +0530

    engxor

--- codechef/ENGXOR.java
+++ codechef/ENGXOR.java
@@ -1,45 +1,89 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
 
 public class ENGXOR {
-    public static boolean is_even(int n) {
-        int count = 0;
-        
-        while(n>0){
-            if((n&1)==1) count++;
-            n = n>>1;
-        }
+    static PrintWriter out;
 
-        if((count&1)==1) return false;
-        return true;
-    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+        FastReader sc = new FastReader();
+        out = new PrintWriter(System.out);
         int t = sc.nextInt();
 
         while(t-->0){
             int n = sc.nextInt();
             int q = sc.nextInt();
 
-            int arr[] = new int[n];
-            for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+            int arr[] = sc.readArray(n);
+            int even=0,odd=0;
+            for(int i=0;i<n;i++) {
+                if(Integer.bitCount(arr[i])%2==0) even++;
+                else odd++;
+            }
 
-            int value,even_count,odd_count,query;
+            int query;
             while(q-->0){
-                even_count=0;
-                odd_count=0;
-
                 query = sc.nextInt();
+                if(Integer.bitCount(query)%2==0)
+                    out.println(even+" "+odd);
+                else
+                    out.println(odd+" "+even);
 
-                for(int i=0;i<n;i++){
-                    value = arr[i]^query;
-
-                    if(is_even(value))  even_count++;
-                    else odd_count++;
-                }
-
-                System.out.println(even_count+" "+odd_count);
             }
         }
+        out.close();
     }
+
+        static class FastReader {
+            BufferedReader br;
+            StringTokenizer st;
+
+            public FastReader() {
+                br = new BufferedReader(
+                        new InputStreamReader(System.in));
+            }
+
+            String next() {
+                while (st == null || !st.hasMoreElements()) {
+                    try {
+                        st = new StringTokenizer(br.readLine());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return st.nextToken();
+            }
+
+            int[] readArray(int n) {
+                int[] a = new int[n];
+                for (int i = 0; i < n; i++) a[i] = nextInt();
+                return a;
+            }
+
+            int nextInt() {
+                return Integer.parseInt(next());
+            }
+
+            long nextLong() {
+                return Long.parseLong(next());
+            }
+
+            double nextDouble() {
+                return Double.parseDouble(next());
+            }
+
+            String nextLine() {
+                String str = "";
+                try {
+                    str = br.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return str;
+            }
+        }
+
 }
