028720141ac6a797867f5baf81932f6ce3326b8c
Akash-Raj-ST <akashraj49070@gmail.com>
2022-12-10 10:58:14 +0530

    .

--- 2to3/WTCH.cpp
+++ 2to3/WTCH.cpp
@@ -5,25 +5,15 @@
 #define ll long long int
 using namespace std;
 
-
 const ll M=1e9+7;
 
 
-bool left(int i,string s){
-    if(i==0) return true;
-
-    if(s[i-1]=='0') return true;
-    return false;
-}
-
-bool right(int i,string s){
-    if(i==s.length()-1) return true;
-
-    if(s[i+1]=='0') return true;
-    return false;
-}
-
 int main(){
+    #ifndef ONLINE_JUDGE
+       freopen("../input.txt","r",stdin); 
+       freopen("../output.txt","w",stdout);
+    #endif
+
     ios_base::sync_with_stdio(false);
     cin.tie(0); 
     cout.tie(0);
@@ -38,30 +28,14 @@ int main(){
         string s;
         cin>>s;
 
-        int p=0;
-
-        for(int i=0;i<n;i++){
-            if(s[i]=='1') p++;
-        }
-
-        if(n%2==0){
-            cout<<n/2-p<<'\n';
-            continue;
-        }else{
-            bool found = false;
-            for(int i=0;i<n;i++){
-                if(s[i]=='1'){
-                    if(i%2==0) cout<<(n/2+1)-p<<'\n';
-                    else cout<<(n/2)-p<<'\n';
-                    found = true;
-                    break;
-                }
-            }
-
-            if(!found){
-                cout<<n/2+1<<'\n';
+        s = "0"+s+"0";
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if(s[i]=='0' && s[i-1]=='0' && s[i+1]=='0'){
+                s[i] = '1';
+                ans++;
             }
         }
-
-    }
+        cout<<ans<<'\n';
+    }   
 }
