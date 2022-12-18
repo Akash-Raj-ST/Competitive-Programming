0e55762ab1c819f9926eda4960b85fa2396dc2d9
Akash-Raj-ST <akashraj49070@gmail.com>
2022-12-18 22:04:36 +0530

    .

---
+++ codechef/dp/ANUWTA.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <queue>
+
+#define ll long long int
+using namespace std;
+
+
+const ll M=1e9+7;
+
+
+int main(){
+     #ifndef ONLINE_JUDGE
+       freopen("../input.txt","r",stdin); 
+       freopen("../output.txt","w",stdout);
+    #endif
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); 
+    cout.tie(0);
+
+    int t;
+    cin>>t;
+
+    while(t--){
+    	ll n;
+    	cin>>n;
+
+    	ll ans = ((n+1)*(n+2))/2;
+    	cout<<ans-1<<'\n';
+    }
+}
