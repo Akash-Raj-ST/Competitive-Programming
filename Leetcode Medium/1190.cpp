1fe73869b47ebba78a3ece2637f70927e36b5ede
Akash-Raj-ST <akashraj49070@gmail.com>
2022-12-31 11:21:20 +0530

    Accepted

--- Leetcode Medium/1190.cpp
+++ Leetcode Medium/1190.cpp
@@ -1,48 +1,26 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
-    string rev(string s){
-        string ans;
-
-        for(int i=s.length()-1;i>=0;i++){
-            ans += s[i];
-        }
-
-        return ans;
-    }
-
     string reverseParentheses(string s) {
-        int count = 0;
+        stack<int> st;
         int n = s.length();
-        string ans;
+
         for(int i=0;i<n;i++){
             if(s[i]=='('){
-                count++;
-                continue;
+                st.push(i);
             }
-
-            int j;
-            string temp;
-            for(j=i;j<n;j++){
-                if(s[j]==')') break;
-                temp += s[j];
+            else if(s[i]==')' && !st.empty()){
+                reverse(s.begin()+st.top(),s.begin()+i);
+                st.pop();
             }
-            i = j;
+        }
 
-            if(count%2==1){
-                ans += rev(temp);
-            }else{
-                ans += temp;
+        string ans;
+
+        for(int i=0;i<n;i++){
+            if(s[i]!='(' && s[i]!=')'){
+                ans += s[i];
             }
         }
         return ans;
     }
 };
-
-int main(){
-    Solution s;
-    cout<<s.reverseParentheses("(ed(et(oc))el)");
-}
