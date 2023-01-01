3ada616ebb0709872155cc90481b647162178c37
Akash-Raj-ST <akashraj49070@gmail.com>
2023-01-01 20:04:22 +0530

    .

---
+++ Leetcode Medium/1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(nums[i])!=mp.end()){
+                return vector<int>{mp[nums[i]],i};
+            }
+
+            mp[target-nums[i]] = i;
+        }
+       
+        return vector<int>(0,0);
+    }
+};
