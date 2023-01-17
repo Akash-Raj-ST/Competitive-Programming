403a9181dd28d2e06175f786a9b4aa832feb4d76
Akash-Raj-ST <akashraj49070@gmail.com>
2023-01-17 23:07:05 +0530

    incomplete

---
+++ Algorithms/TSP.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <queue>
+
+#define ll long long int
+using namespace std;
+
+
+const ll M=1e9+7;
+
+
+int main(){
+    int arr[][4] = {{0,10,15,20},
+    			{5,0,9,10},
+    			{6,13,0,12},
+    			{8,8,9,0}};
+
+   	for(int i=0;i<4;i++){
+   		cout<<arr[i][0]<<' ';
+   	}
+}
