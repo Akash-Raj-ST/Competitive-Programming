8eb176351888e19b66eb72644825d55802937e69
Akash-Raj-ST <akashraj49070@gmail.com>
2023-01-24 19:18:17 +0530

    PASSED

--- 3to4/1/INOI1601.cpp
+++ 3to4/1/INOI1601.cpp
@@ -13,39 +13,65 @@ const ll M=1e9+7;
 
 int main(){
      #ifndef ONLINE_JUDGE
-       freopen("../input.txt","r",stdin); 
-       freopen("../output.txt","w",stdout);
+       freopen("../../input.txt","r",stdin); 
+       freopen("../../output.txt","w",stdout);
     #endif
     
     ios_base::sync_with_stdio(false);
     cin.tie(0); 
     cout.tie(0);
 
-    int t;
-    cin>>t;
+    int t = 1;
 
     while(t--){
-    	int root;
-    	vector<vector<int>> wealth;
-    	vector<vector<int>> arr;
+      int root;
 
-    	int n,manager,we;
-    	cin>>n;
 
-    	for(int i=0;i<n;i++){
-    		cin>>we;
-    		wealth.push_back(we);
-    	}
+      int n,manager,we;
+      cin>>n;
 
-    	for(int i=0;i<n;i++){
-    		cin>>manager;
-    		if(manager==-1){
-    			root = i;
-    		}else{
-    			arr[manager].push_back(i);
-    		}
-    	}
+      vector<vector<int>> arr(n);
+      vector<int> wealth;
+      
 
-    	
+      for(int i=0;i<n;i++){
+        cin>>we;
+        wealth.push_back(we);
+      }
+
+      for(int i=0;i<n;i++){
+        cin>>manager;
+        if(manager==-1){
+          root = i;
+        }else{
+          manager--;
+          arr[manager].push_back(i);
+        }
+      }
+
+      queue<pair<int,int>> q;
+
+    
+      q.push({root,wealth[root]});
+      int diff = 0;
+
+      while(!q.empty()){
+        int curr_node = q.front().first;
+        int node_wealth = q.front().second;
+
+        q.pop();
+
+        for(auto it: arr[curr_node]){
+          if(node_wealth-wealth[it]>diff){
+            diff = node_wealth-wealth[it];
+          }
+          if(wealth[it]>node_wealth)
+            q.push({it,wealth[it]});
+          else
+            q.push({it,node_wealth});
+        }
+      }
+
+      cout<<diff<<'\n';
     }
 }
