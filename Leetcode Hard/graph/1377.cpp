1f767adbfaf02278ea8047e3f992f961872510ce
Akash-Raj-ST <akashraj49070@gmail.com>
2023-02-01 22:45:31 +0530

    Passed

--- Leetcode Hard/graph/1377.cpp
+++ Leetcode Hard/graph/1377.cpp
@@ -1,37 +1,66 @@
 class Solution {
 public:
     double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
-        vector<vector<int>> arr(n);
+        if(n==1) return 1;
+        if(target==1){
+            return 0;
+        }
+
+        vector<vector<int>> arr(n+1);
 
         for(auto it: edges){
             arr[it[0]].push_back(it[1]);
+            arr[it[1]].push_back(it[0]);
         }
+
         queue<vector<double>> q;
         q.push({1,1,0});
 
         bool found = false;
         double ans = 0;
+        
+        vector<bool> visited(n+1,false);
+        visited[1] = true;
 
         while(!q.empty()){
             double node = q.front()[0];
             double prob = q.front()[1];
             double time = q.front()[2];
             cout<<node<<' '<<prob<<' '<<time<<'\n';
-            if(time>t || found) break;
+            if(time>=t || found) break;
 
             q.pop();
 
-            double s = arr[node].size();
+            double s = 0;
+            for(auto it:arr[node]){
+                if(!visited[it]) s++;
+            }
+            
             for(auto it: arr[node]){
+
                 if(it==target){
+                    cout<<"found at "<<node<<'\n';
                     found = true;
                     ans = prob*1/s;
+                    
+                    if(time+1<t)
+                        for(auto it2: arr[it]){
+                            if(!visited[it2]){
+                                ans = 0;
+                                break;
+                            }
+                        }
                     break;
                 }
-                q.push({(double)it,prob*1/s,time+1});
+
+                if(!visited[it]){
+                    q.push({(double)it,prob*1/s,time+1});
+                    visited[it] = true;
+                }
             }
         }
 
+        if(!found) return 0;
         return ans;
 
     }
