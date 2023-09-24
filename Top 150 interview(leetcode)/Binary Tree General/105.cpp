8ed6b5eb43762d20b787b76fcd0aca131bf722e5
Akash-Raj-ST <akashraj49070@gmail.com>
2023-09-24 23:09:15 +0530

    done

---
+++ Top 150 interview(leetcode)/Binary Tree General/105.cpp
@@ -0,0 +1,127 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//inefficient method
+class Solution {
+public:
+
+    vector<int> preorder;
+    vector<int> inorder;
+
+    int pos = 1;
+
+    void partition(int val,vector<int>& left,vector<int>& right,vector<int> main){
+        bool push_left = true;
+
+        for(auto it:main){
+            if(it==val){
+                push_left = false;
+                continue;
+            }
+
+            if(push_left){
+                left.push_back(it);
+            }else{
+                right.push_back(it);
+            }
+        }
+
+    }
+    
+    void solve(TreeNode* node,vector<int> left,vector<int> right){
+
+        vector<int> new_left,new_right;
+
+        // cout<<"for "<<node->val<<'\n';
+        // cout<<"Left: ";
+        // for(auto it:left) cout<<it<<' ';
+        // cout<<"\nRight: ";
+        // for(auto it:right) cout<<it<<' ';
+        // cout<<'\n';
+
+        if(left.size()>0){
+            bool found = false;
+            int left_val;
+
+            for(int i=pos;i<preorder.size();i++){
+                for(int j=0;j<left.size();j++){
+                    if(preorder[i]==left[j]){
+                        found = true;
+                        left_val = left[j];
+                        pos++;
+                        break;
+                    }
+                }
+                if(found) break;
+            }
+
+            node->left =  new TreeNode(left_val);
+            partition(left_val,new_left,new_right,left);
+            solve(node->left,new_left,new_right);
+        }
+
+        new_left.clear();
+        new_right.clear();
+
+        if(right.size()>0){
+            bool found = false;
+            int right_val;
+
+            for(int i=pos;i<preorder.size();i++){
+                for(int j=0;j<right.size();j++){
+                    if(preorder[i]==right[j]){
+                        found = true;
+                        right_val = right[j];
+                        pos++;
+                        break;
+                    }
+                }
+                if(found) break;
+            }
+
+            node->right =  new TreeNode(right_val);
+            partition(right_val,new_left,new_right,right);
+            solve(node->right,new_left,new_right);
+        }
+
+
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+
+        this->preorder = preorder;
+        this->inorder = inorder;
+
+        TreeNode* head = new TreeNode(preorder[0]);
+
+        vector<int> left,right;
+
+        bool push_left = true;
+
+        for(auto it:inorder){
+            if(it==preorder[0]){
+                push_left = false;
+                continue;
+            }
+
+            if(push_left){
+                left.push_back(it);
+            }else{
+                right.push_back(it);
+            }
+        }
+
+        solve(head,left,right);
+
+        return head;
+    }
+};
